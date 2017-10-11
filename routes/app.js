8e18b803d90e2f271e8b86607c166d68aa3b5370
Kevin Oglesby <kevinjoglesby@gmail.com>
2017-10-11 13:58:53 -0700

    styling app template and data binding

--- routes/app.js
+++ routes/app.js
@@ -1,27 +1,8 @@
 var express = require('express');
 var router = express.Router();
-var User = require('../models/user');
 
 router.get('/', function (req, res, next) {
-    User.findOne({}, function(err, doc) {
-        if (err) {
-            return res.send('Error');
-        }
-        res.render('node', {email: doc.email});
-    });
-});
-
-router.post('/', function (req, res, next) {
-  var email = req.body.email;
-  var user = new User({
-      firstName: 'KevKev',
-      lastName: 'Ogle',
-      password: 'secret',
-      email: email
-  });
-  user.save();
-  // This will tell mongoose to store this user object in the users collection in the db
-  res.redirect('/');
+        res.render('index');
 });
 
 module.exports = router;
