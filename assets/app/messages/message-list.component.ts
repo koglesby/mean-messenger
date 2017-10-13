634bb943d5ee869ba3c9362c30e92bc475f89957
Kevin Oglesby <kevinjoglesby@gmail.com>
2017-10-12 17:22:34 -0700

    added message-input and message-list components

---
+++ assets/app/messages/message-list.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { Message } from './message.model';
+
+@Component({
+  selector: 'app-message-list',
+  template: `
+      <div class="col-md-8 col-md-offset-2">
+        <app-message
+          [message]="message"
+          (editClicked)="message.content = $event"
+          *ngFor="let message of messages"></app-message>
+      </div>
+  `
+})
+export class MessageListComponent {
+  messages: Message[] = [
+    new Message('a new message', 'Kevers'),
+    new Message('different message', 'Kev')];
+}
