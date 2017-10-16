c144c2bed865b67351e351cce90894ff4ee3d7ad
Kevin Oglesby <kevinjoglesby@gmail.com>
2017-10-16 15:28:24 -0700

    use http to save messages to db

--- assets/app/messages/message.service.ts
+++ assets/app/messages/message.service.ts
@@ -1,11 +1,25 @@
 import { Message } from './message.model';
+import { Headers, Http, Response } from '@angular/http';
+import { Injectable } from '@angular/core';
+import 'rxjs/Rx'
+import { Observable } from 'rxjs/Observable';
 
+@Injectable()
+// Adds metadata to allow use of Http service
 export class MessageService {
     private messages: Message[] = [];
 
+    constructor(private http: Http) {}
+
     addMessage(message: Message) {
         this.messages.push(message);
-        console.log(this.messages);
+        const body = JSON.stringify(message);
+        const headers = new Headers({'Content-Type': 'application/json'});
+        return this.http.post('http://localhost:3000/message', body, {headers: headers})
+          .map((response: Response) => response.json())
+          .catch((error: Response) => Observable.throw(error.json()));
+        // Should be changed to domain of the real server when app is deployed
+        // sets up an observable that HOLDS the request, and allow us to subscribe to any data the request will return
     }
 
     getMessages() {
