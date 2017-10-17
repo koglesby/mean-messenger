ccc60f17b13117aa573d081d0c43f16fdc02c8a1
Kevin Oglesby <kevinjoglesby@gmail.com>
2017-10-17 16:57:47 -0700

    can delete messages from db

--- routes/messages.js
+++ routes/messages.js
@@ -68,4 +68,33 @@ router.patch('/:id', function(req, res, next) {
     })
 });
 
+router.delete('/:id', function(req, res, next) {
+  Message.findById(req.params.id, function(err, message) {
+    if (err) {
+      return res.status(500).json({
+        title: 'an error occurred',
+        error: err
+      });
+    }
+    if (!message) {
+      return res.status(500).json({
+        title: 'No Message found',
+        error: {message: 'Message not found'}
+      });
+    }
+    message.remove(function(err, result) {
+      if (err) {
+        return res.status(500).json({
+          title: 'an error occurred',
+          error: err
+        });
+      }
+      res.status(200).json({
+        message: 'Deleted message',
+        obj: result
+      });
+    })
+  })
+});
+
 module.exports = router;
