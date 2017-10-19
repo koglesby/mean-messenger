16cb1d4214b992960786f5faefc48847434604fb
Kevin Oglesby <kevinjoglesby@gmail.com>
2017-10-19 16:21:12 -0700

    frontend auth check

--- routes/messages.js
+++ routes/messages.js
@@ -7,18 +7,19 @@ var Message = require('../models/message');
 
 router.get('/', function(req, res, next) {
     Message.find()
-      .exec(function(err, messages) {
-          if (err) {
-              return res.status(500).json({
-                  title: 'an error occurred',
-                  error: err
-              });
-          }
-          res.status(200).json({
-              message: 'Success',
-              obj: messages
-          });
-      })
+      .populate('user', 'firstName')
+        .exec(function(err, messages) {
+            if (err) {
+                return res.status(500).json({
+                    title: 'an error occurred',
+                    error: err
+                });
+            }
+            res.status(200).json({
+                message: 'Success',
+                obj: messages
+            });
+        })
 });
 
 router.use('/', function(req, res, next) {
