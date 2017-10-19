e73145fbe908fdd4b999c8d7f47e3017aaec100c
Kevin Oglesby <kevinjoglesby@gmail.com>
2017-10-19 14:35:43 -0700

    backend route protection

--- routes/messages.js
+++ routes/messages.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var jwt = require('jsonwebtoken');
+var User = require('../models/user');
 
 var Message = require('../models/message');
 
@@ -19,23 +21,49 @@ router.get('/', function(req, res, next) {
       })
 });
 
+router.use('/', function(req, res, next) {
+    jwt.verify(req.query.token, 'secretKey', function(err, decoded) {
+        if (err) {
+            return res.status(401).json({
+                title: 'not authenticated',
+                error: err
+            });
+        }
+        next();
+    })
+});
+
 // only get here from /messages url
 router.post('/', function (req, res, next) {
-    var message = new Message({
-        content: req.body.content
-    });
-    message.save(function(err, result) {
+    var decoded = jwt.decode(req.query.token);
+    User.findById(decoded.user._id, function(err, user) {
         if (err) {
             return res.status(500).json({
                 title: 'an error occurred',
                 error: err
             });
         }
-        res.status(201).json({
-            message: 'Saved the message',
-            obj: result
+        var message = new Message({
+            content: req.body.content,
+            user: user._id
+        });
+        message.save(function(err, result) {
+            if (err) {
+                return res.status(500).json({
+                    title: 'an error occurred',
+                    error: err
+                });
+            }
+            user.messages.push(result);
+            res.status(201).json({
+                message: 'Saved the message',
+                obj: result
+            });
+            user.save();
         });
     });
+
+
 });
 
 router.patch('/:id', function(req, res, next) {
