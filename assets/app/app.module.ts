9fd458f502564a983d3bc68bbd3f4d561e3159b0
Kevin Oglesby <kevinjoglesby@gmail.com>
2017-10-24 14:29:04 -0700

    lazy loading auth module

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -1,13 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from "./app.component";
-import { ReactiveFormsModule } from '@angular/forms';
 import { AuthenticationComponent } from './auth/authentication.component';
 import { HeaderComponent } from './header.component';
 import { routing } from './app.routing';
-import { LogoutComponent } from './auth/logout.component';
-import { SignupComponent } from './auth/signup.component';
-import { SigninComponent } from './auth/signin.component';
 import { HttpModule } from '@angular/http';
 import { AuthService } from './auth/auth.service';
 import { ErrorComponent } from './errors/error.component';
@@ -19,15 +15,11 @@ import { MessageModule } from './messages/message.module';
         AppComponent,
         AuthenticationComponent,
         HeaderComponent,
-        LogoutComponent,
-        SignupComponent,
-        SigninComponent,
         ErrorComponent
     ],
     imports: [
         BrowserModule,
         routing,
-        ReactiveFormsModule,
         HttpModule,
         MessageModule
     ],
